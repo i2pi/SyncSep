628fbdbd89d622efa202c7164584674b5d198c72
Joshua Reich <josh@i2pi.com>
2020-07-05 21:11:04 -0700

    Create syncsep.c

---
+++ syncsep.c
@@ -0,0 +1,162 @@
+#include <xc.h>
+#include <pic.h>
+#include <stdint.h>
+
+/*
+ * SyncSep.c
+ * Joshua Reich / josh@i2pi.com / @i2pi
+ * July 2020.
+ * 
+ * This program is designed to run on the low-cost 8-pin, 8-bit, 8 MHz,
+ * PIC12HV752 MCU. 
+ * 
+ * It takes in the Luma/Sync (green-cable) signal from a 1080p component video
+ * signal, and outputs:
+ * 
+ *   - H Sync      -- HIGH for each incoming h-sync pulse 
+ *   - V Sync      -- HIGH for the first v-sync pulse
+ *   - Line Gate   -- HIGH for the duration of the visible analog video content
+ * 
+ * This is similar to the function of an LM1881, but works for 1080p component
+ * video, instead of NTSC/PAL composite video. 
+ * 
+ * Pinout:
+ * 
+ *                 Vdd  1.    8 GND
+ *    RA5 / V Sync Out  2     7 COG1OUT1 / Line gate
+ *  C1IN1- / Signal IN  3     6 
+ *                      4     5 C1OUT / H sync Out
+ */
+
+
+#pragma config FOSC0 = INT      // FOSC: Oscillator Selection bit (Internal oscillator mode.  I/O function on RA5/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (Watchdog Timer disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (Power-up Timer disabled)
+#pragma config MCLRE = OFF      // MCLR/VPP Pin Function Select bit (MCLR pin is alternate function)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config BOREN = DIS      // Brown-out Reset Enable bits (BOR disabled)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bit (Flash self-write protection off)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function disabled.  CLKOUT pin acts as I/O)
+
+/*
+ * We use the DAC to generate a constant voltage that we compare against to
+ * identify horizontal sync pulses. Using a 5V Vdd, each step in the DAC is
+ * 156mV, so we set this to 3 ==> 0.468V. This works for 1080p component video.
+ * 3 => 3 x 0.156V 
+ */
+#define DAC_SYNC_LEVEL 3  
+
+/*
+ * We use the COG to generate a high level on pin 7 to indicate that we're in 
+ * the portion of the signal that contains analog video. The COG is triggered by
+ * the comparator, which is triggered by a horizontal sync pulse. The video
+ * signal begins ~3uS after this pulse, so we delay the line start to compensate
+ * for this.
+ */
+#define COG_LINE_START_DELAY 7
+
+/*
+ * The analog signal for each line takes about 26uS. We use the HLT timer to
+ * trigger the auto-shutdown of the COG to drive pin 7 low. 
+ */
+#define HLT_LINE_DURATION 55
+
+/*
+ * Each frame includes a number of blank horizontal lines before and after the
+ * actual video frame data. We only want pin 7 to be high during a line that
+ * isn't blank. 
+ */
+#define MIN_LINE_COUNT 35
+#define MAX_LINE_COUNT 1115
+
+uint16_t line_count = 0;  // For counting which line we're currently processing.
+
+void main(void)
+{
+    OSCCON=0b00110000; //8Mhz
+    
+    TRISA5 = 0; // Enable output for RA5 / V Sync 
+       
+    // DAC: Enable, with Full Range, Disable Output, Referenced to Vdd
+    DACCON0 = 0b11000000;
+    DACCON1 = DAC_SYNC_LEVEL; 
+    
+    // Comparator 1
+    TRISA2 = 0; // Allow comparator 1 to drive output
+    TRISA4 = 1; // C1IN1- as Input.
+    CM1CON0 = 0b10101100; // 1: Enabled,
+                          // 0: Non-inverted polarity
+                          // 1: Output enabled
+                          // 0: Output not inverted
+                          // 1: Zero latency filter disabled
+                          // 1: Low speed mode
+                          // 0: Hysterisis enabled
+                          // 0: Asynchronous from Timer1
+    CM1CON1 = 0b00010001; // Read from DAC & C1N1-, No interrupts
+    
+    // Interrupts 
+    INTCON = 0;
+    INTCONbits.GIE = 1; // Enable global interrupts
+    INTCONbits.PEIE = 1; // Enable peripheral interrupts
+    PIE2bits.C1IE = 1;
+    CM1CON1bits.C1INTP = 1;
+    
+    // HLT - for generating the timeout for the line gate pulse
+    HLT1CON0bits.H1ON = 1; // Enable
+    HLT1CON0bits.H1OUTPS = 0; // 1:1 post-scaler
+    HLT1CON0bits.H1CKPS = 0; // 1:1 pre-scaler
+    HLT1CON1bits.H1ERS = 1; // C1OUT Reset
+    HLT1CON1bits.H1FEREN = 1; // Reset on falling edge 
+    HLTPR1 = HLT_LINE_DURATION; // Counter value reset, triggering COG1 shutdown
+    
+    // COG
+    TRISA0 = 0; 
+    COG1CON0bits.G1EN = 1; // Enable COG1
+    COG1CON0bits.G1OE1 = 1; // Enable COG1OUT1 output pin
+    COG1CON0bits.G1CS1 = 0b10; // 8Mhz
+    COG1CON1bits.G1FSIM = 0; // Falling source level sensitive
+    COG1CON1bits.G1RSIM = 0; // Rising source level sensitive
+    COG1CON1bits.G1RS = 0x000; // Rising Source = C1OUT
+    COG1CON1bits.G1FS = 0x000; // Falling Source = C1OUT
+    COG1DBbits.G1DBF = COG_LINE_START_DELAY; // Falling dead band 
+    COG1ASDbits.G1ARSEN = 1; // Enable autorestart
+    COG1ASDbits.G1ASDL1 = 0; // When shutdown, output 0
+    COG1ASDbits.G1ASDSHLT = 1; // Shutdown when HLTMR = HLTPR
+    
+    while(1);
+}
+
+
+void __interrupt() isr(void) {
+    if (PIR2bits.C1IF) {
+        /* 
+         * A sync pulse has happened. This isr has been called ~10us later,
+         * and if the comparator output is also high right now, then we're
+         * in the middle of a vertical sync pulse, so output that to RA5 and 
+         * reset the line counter.
+         */
+        if (CMOUTbits.MCOUT1) {
+            // Only output a pulse for the first in the train of 5
+            if (line_count > 5) PORTAbits.RA5 = 1; 
+            line_count = 0;
+        } else {
+            PORTAbits.RA5 = 0;
+            line_count++;
+        }
+        
+        PIR2bits.C1IF = 0;
+        
+         if ((line_count > MAX_LINE_COUNT) || (line_count < MIN_LINE_COUNT)) {
+            /* 
+             * Disable the COG responsible for outputting the Line Gate signal,
+             * as we are outside of the viewable 1080 lines in the frame.
+             */
+            COG1CON0bits.G1EN = 0;
+            COG1CON0bits.G1OE1 = 0;
+            PORTAbits.RA0 = 0;
+        } else {
+            COG1CON0bits.G1EN = 1;
+            COG1CON0bits.G1OE1 = 1;
+        }
+    }
+}
